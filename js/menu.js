741255d418436530e4d5a925fcbf5d8bc6d74f30
JRDLGH <ipes.lagache@gmail.com>
2018-04-12 16:47:55 +0200

    Correcting navigation

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 $(function(){
+    console.log(getSectionsPositions());
     var anchorAnimate = false;
     $('.menu-icon').click(function(){
         $(this).toggleClass('is-opened');
@@ -15,6 +16,19 @@ $(function(){
         if(($('html').offset().top + 40) > $('html').scrollTop()){
             $('header').removeClass('fixed');
         }
+        if(true){
+            // switch(true){
+            //     case 1:;
+            //     break;
+            //     case 2:;
+            //     break;
+            //     case 3:;
+            //     break;
+            //     case 4:;
+            //     break;
+            //     default:;
+            // }
+        }
     });
     $(window).resize(function(){
         if(window.innerWidth >= 1024){
@@ -28,17 +42,27 @@ $(function(){
         evt.preventDefault();
         if(!$(evt.target.hash).is(':animated')){
             var position = $(this.hash).offset().top;
+            var navHeight = 0;
+            if(position > 50){
+                var navHeight = getHeight($('header')) ? getHeight($('header')): 0;
+            }
             if(isMobile()){
                 //Close the menu
                 $('.menu-icon').click();
             }
             toggleActive($(this));
             $('html, body').animate({
-                scrollTop: position
+                scrollTop: position - navHeight
             },1000);
         }
-    })
+    });
 });
+function getHeight(elem){
+    if(elem){
+        return elem.outerHeight();
+    }
+    return 0;
+}
 
 function isMobile(){
     var state= false;
@@ -47,14 +71,25 @@ function isMobile(){
     }
     return state;
 }
+
 function toggleActive(link){
     var activeLink = $('header nav #menu li').children('.active');
-    console.log(activeLink);
     if(activeLink.length > 0){
-        console.log("Active link present and removed");
         activeLink.removeClass('active');
     }
     if(!link.hasClass('active')){
         link.addClass('active');
     }
+}
+
+function getSectionsPositions(){
+    var positions = {};
+    var sections = $('section');
+    sections.each(function(index){
+        if($(sections[index]).attr('id')){
+            positions[$(sections[index]).attr('id')] = $(sections[index]).offset
+            ().top;
+        }
+    });
+    return positions;
 }
