8a054e215a2c2847a043219c82ae95f959090f80
Jordan Lagache <j.lagache@bigbadwolf.be>
2018-04-17 17:30:44 +0200

    Adding element to slider

--- js/slider-details.js
+++ js/slider-details.js
@@ -1,6 +1,9 @@
 $(function(){
     $('.btn-discover.details').click(function(evt){
         evt.preventDefault();
+        if($('.slide-details-content').hasClass('is-opened')){
+            $('.slide-details-content').removeClass('is-opened');
+        }
         $(this.hash).toggleClass('is-opened');
     });
 });
