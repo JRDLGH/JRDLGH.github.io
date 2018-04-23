04f410458ed29e755c4ceb526619a45ceba9d988
Jordan Lagache <j.lagache@bigbadwolf.be>
2018-04-23 10:09:55 +0200

    Resolving slider details bug

--- js/slider-details.js
+++ js/slider-details.js
@@ -1,14 +1,16 @@
 $(function(){
     $('.btn-discover.details').click(function(evt){
         evt.preventDefault();
-        if($('.slide-details-content').hasClass('is-opened')){
+        console.log($('.slide-details-content.is-opened'));
+        console.log($(this.hash));
+        console.log($('.slide-details-content.is-opened')[0] == $(this.hash)[0]);
+        if($('.slide-details-content.is-opened')[0] == $(this.hash)[0]){
+            $('.slide-details-content.is-opened').removeClass('is-opened');
+        }else if($('.slide-details-content').hasClass('is-opened')){
             $('.slide-details-content').removeClass('is-opened');
+            $(this.hash).toggleClass('is-opened');
         }
-        console.log($(this.hash).child('.is-opened'));
-        if($(this.hash).hasClass('is-opened')){
-            console.log('Already opened');
-            $('.slide-details-content').removeClass('is-opened');            
-        }else{
+        else{
             $(this.hash).toggleClass('is-opened');
         }
     });
